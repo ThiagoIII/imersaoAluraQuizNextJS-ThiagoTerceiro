9e19847482a13c0517afe56093231f7624045245
ThiagoIII <thiagoiii@hotmail.com>
2021-01-29 15:18:23 -0300

    other folders

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
 import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import db from '../db.json'
+import QuizBackground from '../src/components/QuizBackground/QuizBackGround'
+import QuizContainer from '../src/components/QuizContainer/QuizContainer'
+import Widget from '../src/components/Widget/Widgets'
+import Footer from '../src/components/Footer/Footer'
+import GitHubCorner from '../src/components/GitHubCorner/GitHubCorner'
 
 export default function Home() {
-  return <Title>My page</Title>
+    return (
+        <QuizBackground backgroundImage={db.bg}>
+            <QuizContainer>
+                <Widget>
+                    <Widget.Header>
+                        <h1>Titulo widget 1</h1>
+                    </Widget.Header>
+                    <Widget.Content>
+                        <p>Paragrafo wid1</p>
+                    </Widget.Content>
+                </Widget>
+                <Widget>
+                    <Widget.Header>
+                        <h1>Titulo widget 2</h1>
+                    </Widget.Header>
+                    <Widget.Content>
+                        <p>Paragrafo wid1</p>
+                    </Widget.Content>
+                </Widget>
+                <Footer />
+            </QuizContainer>
+            <GitHubCorner projectUrl={'https://github.com/ThiagoIII/'} />
+        </QuizBackground>
+    )
 }
