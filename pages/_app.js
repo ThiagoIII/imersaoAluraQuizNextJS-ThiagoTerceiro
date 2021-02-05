c213cabd732241aba639f0a505c69b44123713b5
ThiagoIII <thiagoiii@hotmail.com>
2021-02-05 12:53:13 -0300

    general code, components

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,83 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import Head from 'next/head'
 import db from '../db.json'
 
-const GlobalStyle = createGlobalStyle`* {
-  box-sizing: border-box;
-}
-body {
-  margin: 0;
-  padding: 0;
-  /* New styles */
-  display: flex;
-  flex-direction: column;
-  font-family: 'Lato', sans-serif;
-  // Deixa branco no começo
-  color: ${({ theme }) => theme.colors.contrastText};
-}
-html, body {
-  min-height: 100vh;
-}
-#__next {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-}
+const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        margin: 0;
+        padding: 0;
+        /* New styles */
+        display: flex;
+        flex-direction: column;
+        font-family: 'Lato', sans-serif;
+        // Deixa branco no começo
+        color: ${({ theme }) => theme.colors.contrastText};
+    }
+    html, body {
+        min-height: 100vh;
+    }
+    #__next {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+    }
 `
 
-const theme = db.theme
+const { theme } = db
 
 export default function App({ Component, pageProps }) {
     return (
         <>
+            <Head>
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+                    rel="stylesheet"
+                />
+                <title>Alura Imersão NextJS Quiz Thiago Terceiro</title>
+                <meta charSet="utf-8" />
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0, width=device-width"
+                />
+                <meta
+                    property="og:title"
+                    content="Alura Imersão NextJS Quiz by Thiago Terceiro"
+                />
+                <meta property="og:type" content="website" />
+                <meta
+                    property="og:url"
+                    content="https://imersao-alura-quiz-next-js-thiago-terceiro.vercel.app/"
+                />
+                <meta
+                    property="og:image"
+                    content="https://wallpapercave.com/wp/wp3388505.jpg"
+                />
+                <meta
+                    property="og:description"
+                    content="Alura Imersão NextJS Quiz by Thiago Terceiro"
+                />
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta
+                    property="twitter:url"
+                    content="https://imersao-alura-quiz-next-js-thiago-terceiro.vercel.app/"
+                />
+                <meta
+                    property="twitter:title"
+                    content="Alura Imersão NextJS Quiz by Thiago Terceiro"
+                />
+                <meta
+                    property="twitter:description"
+                    content="Alura Imersão NextJS Quiz by Thiago Terceiro"
+                />
+                <meta
+                    property="twitter:image"
+                    content="https://wallpapercave.com/wp/wp3388505.jpg"
+                />
+            </Head>
             <ThemeProvider theme={theme}>
                 <GlobalStyle />
                 <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,123 @@
+import React from 'react'
 import styled from 'styled-components'
+import Head from 'next/head'
+import { motion } from 'framer-motion'
+import { useRouter } from 'next/router'
+
 import db from '../db.json'
-import QuizBackground from '../src/components/QuizBackground/QuizBackGround'
-import QuizContainer from '../src/components/QuizContainer/QuizContainer'
 import Widget from '../src/components/Widget/Widgets'
+import Link from '../src/components/Link/Link'
+import QuizLogo from '../src/components/QuizLogo/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground/QuizBackGround'
 import Footer from '../src/components/Footer/Footer'
 import GitHubCorner from '../src/components/GitHubCorner/GitHubCorner'
+import Input from '../src/components/Input/Input'
+import Button from '../src/components/Button/Button'
+
+const QuizContainer = styled.div`
+    width: 100%;
+    max-width: 350px;
+    padding-top: 45px;
+    margin: auto 10%;
+    @media screen and (max-width: 500px) {
+        margin: auto;
+        padding: 15px;
+    }
+`
 
 export default function Home() {
+    const router = useRouter()
+    const [name, setName] = React.useState('')
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        router.push(`/quiz?name=${name}`)
+    }
+
     return (
         <QuizBackground backgroundImage={db.bg}>
+            <Head>
+                <title>AluraQuiz -{db.title}</title>
+            </Head>
             <QuizContainer>
-                <Widget>
+                <QuizLogo />
+                <Widget
+                    as={motion.section}
+                    transition={{ delay: 0, duration: 0.5 }}
+                    variants={{
+                        show: { opacity: 1, y: '0' },
+                        hidden: { opacity: 0, y: '100%' }
+                    }}
+                    initial="hidden"
+                    animate="show"
+                >
                     <Widget.Header>
-                        <h1>Titulo widget 1</h1>
+                        <h1>{db.title}</h1>
                     </Widget.Header>
                     <Widget.Content>
-                        <p>Paragrafo wid1</p>
+                        <p>{db.description}</p>
+                        <form onSubmit={e => handleSubmit(e)}>
+                            <input
+                                name="nomeDoUsuario"
+                                onChange={e => setName(e.target.value)}
+                                type="text"
+                                placeholder="Digite seu nome aqui"
+                                value={name}
+                            />
+                            <Button type="submit" disabled={name.length === 0}>
+                                {`Jogar ${name}`}
+                            </Button>
+                        </form>
                     </Widget.Content>
                 </Widget>
-                <Widget>
-                    <Widget.Header>
-                        <h1>Titulo widget 2</h1>
-                    </Widget.Header>
+                <Widget
+                    as={motion.section}
+                    transition={{ delay: 0.5, duration: 0.5 }}
+                    variants={{
+                        show: { opacity: 1 },
+                        hidden: { opacity: 0 }
+                    }}
+                    initial="hidden"
+                    animate="show"
+                >
                     <Widget.Content>
-                        <p>Paragrafo wid1</p>
+                        <h1>Quizes da Galera</h1>
+
+                        <ul>
+                            {db.external.map(linkExterno => {
+                                const [
+                                    projectName,
+                                    githubUser
+                                ] = linkExterno
+                                    .replace(/\//g, '')
+                                    .replace('https:', '')
+                                    .replace('.vercel.app', '')
+                                    .split('.')
+
+                                return (
+                                    <li key={linkExterno}>
+                                        <Widget.Topic
+                                            as={Link}
+                                            href={`/quiz/${projectName}___${githubUser}`}
+                                        >
+                                            {`${githubUser}/${projectName}`}
+                                        </Widget.Topic>
+                                    </li>
+                                )
+                            })}
+                        </ul>
                     </Widget.Content>
                 </Widget>
-                <Footer />
+                <Footer
+                    as={motion.footer}
+                    transition={{ delay: 0.5, duration: 0.5 }}
+                    variants={{
+                        show: { opacity: 1 },
+                        hidden: { opacity: 0 }
+                    }}
+                    initial="hidden"
+                    animate="show"
+                />
             </QuizContainer>
             <GitHubCorner projectUrl={'https://github.com/ThiagoIII/'} />
         </QuizBackground>
